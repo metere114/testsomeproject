38bd1ec51e538dd039e4454030c31b97184dc05f
Vinh Duc Nguyen <dragongreen97@gmail.com>
2022-09-16 21:36:08 +0200

    checked txt file

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
-import java.util.Random;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-
+        Scanner scanner = new Scanner(new File("C:\\Users\\Admin\\OneDrive\\Máy tính\\New folder\\Hangman.txt"));
+        System.out.println(scanner.hasNext());
+        System.out.println(scanner.nextLine());
     }
 }
