3534851adad90ab5ad30fa118642a677c232e80f
Vinh Duc Nguyen <dragongreen97@gmail.com>
2022-09-16 22:15:44 +0200

    create while loop

--- src/App.java
+++ src/App.java
@@ -6,7 +6,9 @@ public class App {
     public static void main(String[] args) throws FileNotFoundException {
 
         Scanner scanner = new Scanner(new File("C:\\Users\\Admin\\OneDrive\\Máy tính\\New folder\\Hangman.txt"));
-        System.out.println(scanner.hasNext());
-        System.out.println(scanner.nextLine());
+        while (scanner.hasNext()) {
+            System.out.println(scanner.nextLine());
+
+        }
     }
 }
