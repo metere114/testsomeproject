0dfb8670c591fcb3d39d409895963870fe12c5ca
Vinh Duc Nguyen <dragongreen97@gmail.com>
2022-11-11 15:49:27 +0100

    Complete Hangman game

--- src/App.java
+++ src/App.java
@@ -1,14 +1,125 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) throws FileNotFoundException {
 
-        Scanner scanner = new Scanner(new File("C:\\Users\\Admin\\OneDrive\\Máy tính\\New folder\\Hangman.txt"));
-        while (scanner.hasNext()) {
-            System.out.println(scanner.nextLine());
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("1 or 2 player ?");
+        String players = keyboard.nextLine();
+        String word;
+        if (players.equals("1")){
+            Scanner scanner = new Scanner(new File("C:\\Users\\Admin\\OneDrive\\Máy tính\\New folder\\Hangman.txt"));
+
+
+
+            List<String> words = new ArrayList<>();
+            while (scanner.hasNext()) {
+                words.add(scanner.nextLine());
+
+            }
+
+            Random rand = new Random();
+
+            word = words.get(rand.nextInt(words.size()));
+        }
+        else {
+            System.out.println("player 1, please enter your word:");
+            word = keyboard.nextLine();
+            System.out.println("\n\n\n\n");
+            System.out.println("Ready for player 2! Good luck!)");
+        }
+
+        //System.out.println(word);
+
+        List<Character> playerGuesses = new ArrayList<>();
+
+
+        int wrongCount = 0;
+        while (true) {
+
+            printHangedMan(wrongCount);
+
+            if (wrongCount>=6){
+                System.out.println("You lose!");
+                System.out.println("The hidden word was: "+word);
+                break;
+            }
+
+            printWordState(word, playerGuesses);
+            if (!getPlayerGuess(keyboard, word, playerGuesses)){
+                wrongCount++;
+            }
+
+            if(printWordState(word, playerGuesses)){
+                System.out.println("You win!");
+                break;
+            }
+            System.out.println("Please enter your guess for the word:");
+            if(keyboard.nextLine().equals(word)){
+                System.out.println("You win!");
+                break;
+            }
+            else {
+                System.out.println("Nope! Try again.");
+            }
 
         }
+
+        //System.out.println("You win!");
+    }
+
+    private static void printHangedMan(int wrongCount) {
+        System.out.println(" -------");
+        System.out.println(" |     |");
+        if (wrongCount >=1){
+            System.out.println(" O");
+        }
+        if(wrongCount >=2){
+            System.out.print("\\ ");
+            if(wrongCount >=3){
+                System.out.println("/");
+            }
+            else {
+                System.out.println("");
+            }
+        }
+        if (wrongCount >=4){
+            System.out.println(" |");
+        }
+        if(wrongCount >=5){
+            System.out.print("/ ");
+            if(wrongCount >=6){
+                System.out.println("\\");
+            }
+            else {
+                System.out.println("");
+            }
+        }
+        //System.out.println("");
+        System.out.println("");
+    }
+
+    private static boolean getPlayerGuess(Scanner keyboard, String word, List<Character> playerGuesses) {
+        System.out.println("Please enter a letter: ");
+        String letterGuess = keyboard.nextLine();
+        playerGuesses.add(letterGuess.charAt(0));
+
+        return word.contains(letterGuess);
+    }
+
+    private static boolean printWordState(String word, List<Character> playerGuesses) {
+        int correctCount = 0;
+        for (int i = 0; i < word.length(); i++){
+            if (playerGuesses.contains(word.charAt(i))){
+                System.out.print(word.charAt(i));
+                correctCount++;
+            }else {
+                System.out.print("-");
+            }
+        }
+        System.out.println();
+        return (word.length()==correctCount);
     }
 }
